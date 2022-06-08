beb728d7e0f17bd10e7d010f8df08abccfe0b4c9
MonikaMan <moninemeti94@gmail.com>
2022-06-08 15:56:59 +0300

    homework4

---
+++ homework4.js
@@ -0,0 +1,151 @@
+// 1. Виведіть користувачу попап використовуючи функцію "alert" який містить довільне повідомлення.
+alert('Hello')
+undefined
+// 2. Виведіть користувачу попап використовуючи функцію "prompt" який містить довільне питання і не має дефолтного значення. Результат присвойте у змінну.
+// *Виведіть змінну в консоль за допомогою "console.log"
+let popap= prompt('як тебе звати?' ,'');
+undefined
+console.log(popap)
+ monika
+undefined
+// 3. Виведіть користувачу попап використовуючи функцію "prompt" який містить довільне питання і має дефолтнe значення "13". Результат присвойте у змінну.
+// *Виведіть змінну в консоль за допомогою "console.log"
+let apple= prompt('скільк коштує кг яблука?',13);
+undefined
+console.log(apple)
+ 13
+undefined
+// 4. Виведіть користувачу попап використовуючи функцію "confirm" який містить довільне питання. Результат присвойте у змінну.
+// *Виведіть змінну в консоль за допомогою "console.log"
+let car1 = confirm('це червона машина?')
+undefined
+console.log(car1)
+true
+undefined
+// 5. Виведіть користувачу попап використовуючи функцію "confirm" який містить довільне питання. Результат присвойте у змінну.
+// *Виведіть змінну в консоль за допомогою "console.log"
+// *Використовуючи "if" виведіть в консоль "1" якщо відповідь на питання була позитивна
+let car = confirm ('the car is red?')
+console.log(car)
+if ('car is red'){alert('1')}
+true
+// 6. Виведіть користувачу попап використовуючи функцію "prompt" який містить довільне питання і не має дефолтного значення.
+// Результат присвойте у змінну.
+// *Виведіть змінну в консоль за допомогою "console.log"
+// *Використовуючи "if else" виведіть в консоль "1" або "2"
+let apple= prompt('скільк коштує кг яблука?');
+undefined
+console.log(apple)
+ 3
+let apple =3
+undefined
+if(apple ===3){console.log('1')} else {console.log('2')}
+ 1
+undefined
+let apple =5
+undefined
+if(apple ===3){console.log('1')} else {console.log('2')}
+ 2
+undefined
+// 7. Виведіть користувачу попап використовуючи функцію "prompt" який містить довільне питання і не має дефолтного значення. 
+//Результат присвойте у змінну.
+// *Виведіть змінну в консоль за допомогою "console.log"
+// *Використовуючи "if else" виведіть в консоль "1" або "2" або "3"
+let apple= prompt('скільк коштує кг яблука?');
+undefined
+console.log(apple)
+3
+let apple =3
+ if(apple ===3){console.log('1')} else if (apple === 8) {console.log ('2')} else{console.log('3')}
+ 1
+undefined
+let apple =8
+undefined
+if(apple ===3){console.log('1')} else if (apple === 8) {console.log ('2')} else{console.log('3')}
+ 2
+undefined
+let apple = 'fgfg'
+undefined
+if(apple ===3){console.log('1')} else if (apple === 8) {console.log ('2')} else{console.log('3')}
+ 3
+undefined
+// 8. Виведіть користувачу попап використовуючи функцію "confirm" який містить довільне питання. Результат присвойте у змінну.
+// *Виведіть змінну в консоль за допомогою "console.log"
+// *Використовуючи "switch" виведіть в консоль "1" якщо відповідь на питання була позитивна
+let car = confirm ('the car is red?')
+undefined
+console.log(car)
+true
+undefined
+switch (car){case true: alert('1')}
+undefined
+// 9. Виведіть користувачу попап використовуючи функцію "prompt" який містить довільне питання і не має дефолтного значення.
+// Результат присвойте у змінну.
+// *Виведіть змінну в консоль за допомогою "console.log"
+// *Використовуючи "switch" виведіть в консоль "1" або "2"
+let bus=prompt('скільки км проїхав буc?')
+undefined
+console.log(bus)
+VM4851:1 20
+undefined
+let bus=20
+undefined
+switch (bus){case 20: console.log('1');break; case 15:console.log('2');break;}
+ 1
+undefined
+let bus=15
+undefined
+switch (bus){case 20: console.log('1');break; case 15:console.log('2');break;}
+ 2
+
+ // 10. Виведіть користувачу попап використовуючи функцію "prompt" який містить довільне питання і не має дефолтного значення.
+ // Результат присвойте у змінну.
+// *Виведіть змінну в консоль за допомогою "console.log"
+// *Використовуючи "switch" виведіть в консоль "1" або "2" або "3"
+let bus=prompt('скільки км проїхав буc?')
+undefined
+console.log(bus)
+ 20
+undefined
+let bus=20
+undefined
+switch (bus){case 20: console.log('1');break; case 15:console.log('2');break; case10:console.log('3');break;}
+ 1
+undefined
+let bus=15
+undefined
+switch (bus){case 20: console.log('1');break; case 15:console.log('2');break; case10:console.log('3');break;}
+ 2
+undefined
+let bus =10
+undefined
+switch (bus){case 20: console.log('1');break; case 15:console.log('2');break; case 10:console.log('3');break;}
+3
+undefined
+// 11. Виведіть користувачу попап використовуючи функцію "prompt" який містить довільне питання і не має дефолтного значення. Результат присвойте у змінну.
+// *Виведіть змінну в консоль за допомогою "console.log"
+// *Використовуючи "switch" виведіть в консоль "1" або "2" або "3", виконайте те саме за допомогою "if else";
+// *Зробіть два різні приклади
+//!!!!!!для switch готовий варіант в попередньому завданні!!!!
+//для if else ! 
+let bus=prompt('скільки км проїхав буc?')
+undefined
+console.log(bus)
+VM6160:1 20
+undefined
+let bus=20
+undefined
+if(bus===20){console.log('1')} else if (bus === 15) {console.log ('2')} else{console.log('3')}
+ 1
+undefined
+let bus=15
+undefined
+if(bus===20){console.log('1')} else if (bus === 15) {console.log ('2')} else{console.log('3')}
+
+2
+undefined
+let bus= 215
+undefined
+if(bus===20){console.log('1')} else if (bus === 15) {console.log ('2')} else{console.log('3')}
+ 3
+undefined
